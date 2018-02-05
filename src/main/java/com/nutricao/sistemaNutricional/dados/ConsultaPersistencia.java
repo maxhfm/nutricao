3d681c13959368a5523e872e35d325baab8444e3
maxhfm <30988971+maxhfm@users.noreply.github.com>
2018-02-05 12:39:10 -0200

    Update ConsultaPersistencia.java

--- src/main/java/com/nutricao/sistemaNutricional/dados/ConsultaPersistencia.java
+++ src/main/java/com/nutricao/sistemaNutricional/dados/ConsultaPersistencia.java
@@ -5,5 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.nutricao.sistemaNutricional.model.Consulta;
 
 public interface ConsultaPersistencia extends JpaRepository<Consulta, Long>{
+  
+  @Query("")
+  List<>
+  
+  @Query("select nome from Paciente p where p.nome like ?1")
+  List<Paciente> pesquisaPorNome(String nome);
+  
+  
 
+  
+  
 }
