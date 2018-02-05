c5834e5356be244170c30049396dacf92adb3a91
maxhfm <30988971+maxhfm@users.noreply.github.com>
2018-02-05 14:55:14 -0200

    Update PacientePersistencia.java

--- src/main/java/com/nutricao/sistemaNutricional/dados/PacientePersistencia.java
+++ src/main/java/com/nutricao/sistemaNutricional/dados/PacientePersistencia.java
@@ -8,6 +8,9 @@ import com.nutricao.sistemaNutricional.model.Paciente;
 @Repository
 public interface PacientePersistencia extends JpaRepository <Paciente, Long>{
   
+  @Query("select * from paciente p inner join consulta c on p.id=c.id")
+  List<Paciente> consultaPaciente();
+  
   @Query("select nome from Paciente p where p.nome like ?1")
   List<Paciente> pesquisaPorNome(String nome);
 
